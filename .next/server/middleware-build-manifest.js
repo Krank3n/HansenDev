b39580f0eb61e84d92513d60214bebbedf279cfc
Thomas A Hansen <45204791+Krank3n@users.noreply.github.com>
2025-10-23 11:59:30 +1000

    Added in More Pages

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -24,11 +24,6 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/projects/quotemate.js"
-    ],
-    "/projects/quotemate-privacy": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/projects/quotemate-privacy.js"
     ]
   },
   "ampFirstPages": []
